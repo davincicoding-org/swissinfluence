da95be23cc6950c51ac98f8faeb1f5419af585f1
Michael Zeltner <michael.zeltner@me.com>
2025-05-23 05:01:15 +0200

    feat: add cooperation interest selection to certified influencers

--- src/cms/resources/certified_influencers.tsx
+++ src/cms/resources/certified_influencers.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { ImageInput } from "@davincicoding/cms/image";
 import { TranslatableTextInput } from "@davincicoding/cms/text";
-import { Box } from "@mui/material";
+import { useDebouncedValue } from "@mantine/hooks";
+import { Autocomplete, Box, TextField } from "@mui/material";
+import { IconX } from "@tabler/icons-react";
 import {
   AutocompleteArrayInput,
   AutocompleteInput,
@@ -10,16 +13,25 @@ import {
   Edit,
   List,
   minLength,
-  ReferenceArrayInput,
+  TextField as RATextField,
   ReferenceField,
   ReferenceInput,
   required,
   SimpleForm,
-  TextField,
   TranslatableInputs,
+  useCreate,
+  useDelete,
+  useGetList,
+  useGetMany,
+  useGetManyReference,
+  useRecordContext,
 } from "react-admin";
 
-import type { categories } from "@/database/schema";
+import type {
+  categories,
+  certifiedInfluencers,
+  influencerCategories,
+} from "@/database/schema";
 import { Locale } from "@/i18n/config";
 import { CANTON_CHOICES } from "@/utils/cantons";
 import { LANGUAGE_CHOICES } from "@/utils/languages";
@@ -37,7 +49,7 @@ export function CertifiedInfluencersList() {
           source="influencer"
           link={false}
         >
-          <TextField source="name" />
+          <RATextField source="name" />
         </ReferenceField>
       </Datagrid>
     </List>
@@ -49,98 +61,7 @@ export function CertifiedInfluencersList() {
 export function CertifiedInfluencersCreate() {
   return (
     <Create redirect="list">
-      <SimpleForm>
-        <Box
-          sx={{
-            display: "grid",
-            gridTemplateColumns: "300px 1fr",
-            gap: 3,
-            mb: 1,
-            width: "100%",
-          }}
-        >
-          <Box>
-            <ImageInput
-              label={false}
-              source="image"
-              validate={required("Add an image")}
-              optimization={{
-                compression: "photography",
-              }}
-            />
-            {/* FIXME: this is not working */}
-            <ReferenceArrayInput
-              source="influencer"
-              reference="influencer_categories"
-            >
-              <AutocompleteArrayInput
-                disabled
-                label="Cooperation Interests"
-                variant="outlined"
-                // optionText={({ title }: typeof categories.$inferSelect) =>
-                //   title.en
-                // }
-                helperText={false}
-                validate={[
-                  required("Add a category"),
-                  minLength("Add at least one category"),
-                ]}
-              />
-            </ReferenceArrayInput>
-
-            <DateInput
-              source="birthdate"
-              variant="outlined"
-              helperText={false}
-              validate={required("Add the influencer's birthdate")}
-            />
-            <AutocompleteArrayInput
-              source="languages"
-              variant="outlined"
-              choices={LANGUAGE_CHOICES}
-              helperText={false}
-              validate={[
-                required("Add a language"),
-                minLength("Add at least one language"),
-              ]}
-            />
-            <AutocompleteInput
-              source="residence"
-              variant="outlined"
-              choices={CANTON_CHOICES}
-              helperText={false}
-              validate={required("Add the influencer's residence")}
-            />
-          </Box>
-          <Box>
-            <ReferenceInput
-              label="Influencer"
-              source="influencer"
-              reference="influencers"
-            >
-              <AutocompleteInput
-                variant="outlined"
-                helperText={false}
-                validate={required("Pick an influencer")}
-              />
-            </ReferenceInput>
-            <TranslatableInputs locales={Locale.options}>
-              <TranslatableTextInput
-                source="bio"
-                helperText={false}
-                multiline
-                validate={required("Describe the influencer")}
-              />
-              <TranslatableTextInput
-                source="other_interests"
-                helperText="Add each interest on a new line"
-                multiline
-                validate={required("Add the influencer's interests")}
-              />
-            </TranslatableInputs>
-          </Box>
-        </Box>
-      </SimpleForm>
+      <CertifiedInfluencersFrom />
     </Create>
   );
 }
@@ -150,99 +71,205 @@ export function CertifiedInfluencersCreate() {
 export function CertifiedInfluencersEdit() {
   return (
     <Edit>
-      <SimpleForm>
-        <Box
-          sx={{
-            display: "grid",
-            gridTemplateColumns: "300px 1fr",
-            gap: 3,
-            mb: 1,
-            width: "100%",
-          }}
-        >
-          <Box>
-            <ImageInput
-              label={false}
-              source="image"
-              validate={required("Add an image")}
-              optimization={{
-                compression: "photography",
-              }}
-            />
-            {/* FIXME: this is not working */}
-            <ReferenceArrayInput
-              source="influencer"
-              reference="influencer_categories"
-            >
-              <AutocompleteArrayInput
-                disabled
-                label="Cooperation Interests"
-                variant="outlined"
-                // optionText={({ title }: typeof categories.$inferSelect) =>
-                //   title.en
-                // }
-                // helperText={false}
-                helperText="WORK IN PROGRESS"
-                validate={[
-                  required("Add a category"),
-                  minLength("Add at least one category"),
-                ]}
-              />
-            </ReferenceArrayInput>
-
-            <DateInput
-              source="birthdate"
-              variant="outlined"
-              helperText={false}
-              validate={required("Add the influencer's birthdate")}
-            />
-            <AutocompleteArrayInput
-              source="languages"
-              variant="outlined"
-              choices={LANGUAGE_CHOICES}
-              helperText={false}
-              validate={[
-                required("Add a language"),
-                minLength("Add at least one language"),
-              ]}
-            />
-            <AutocompleteInput
-              source="residence"
-              variant="outlined"
-              choices={CANTON_CHOICES}
-              helperText={false}
-              validate={required("Add the influencer's residence")}
-            />
-          </Box>
-          <Box>
-            <ReferenceInput
-              label="Influencer"
-              source="influencer"
-              reference="influencers"
-            >
-              <AutocompleteInput
-                variant="outlined"
-                helperText={false}
-                validate={required("Pick an influencer")}
-              />
-            </ReferenceInput>
-            <TranslatableInputs locales={Locale.options}>
-              <TranslatableTextInput
-                source="bio"
-                helperText={false}
-                multiline
-                validate={required("Describe the influencer")}
-              />
-              <TranslatableTextInput
-                source="other_interests"
-                helperText="Add each interest on a new line"
-                multiline
-                validate={required("Add the influencer's interests")}
-              />
-            </TranslatableInputs>
-          </Box>
-        </Box>
-      </SimpleForm>
+      <CertifiedInfluencersFrom />
     </Edit>
+  );
+}
+
+// MARK: From
+
+function CertifiedInfluencersFrom() {
+  return (
+    <SimpleForm>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "300px 1fr",
+          gap: 3,
+          mb: 1,
+          width: "100%",
+        }}
+      >
+        <Box>
+          <ImageInput
+            label={false}
+            source="image"
+            validate={required("Add an image")}
+            optimization={{
+              compression: "photography",
+            }}
+          />
+          <CategoriesSection />
+
+          <DateInput
+            source="birthdate"
+            variant="outlined"
+            helperText={false}
+            validate={required("Add the influencer's birthdate")}
+          />
+          <AutocompleteArrayInput
+            source="languages"
+            variant="outlined"
+            choices={LANGUAGE_CHOICES}
+            helperText={false}
+            validate={[
+              required("Add a language"),
+              minLength("Add at least one language"),
+            ]}
+          />
+          <AutocompleteInput
+            source="residence"
+            variant="outlined"
+            choices={CANTON_CHOICES}
+            helperText={false}
+            validate={required("Add the influencer's residence")}
+          />
+        </Box>
+        <Box>
+          <ReferenceInput
+            label="Influencer"
+            source="influencer"
+            reference="influencers"
+          >
+            <AutocompleteInput
+              variant="outlined"
+              helperText={false}
+              validate={required("Pick an influencer")}
+            />
+          </ReferenceInput>
+          <TranslatableInputs locales={Locale.options}>
+            <TranslatableTextInput
+              source="bio"
+              helperText={false}
+              multiline
+              validate={required("Describe the influencer")}
+            />
+            <TranslatableTextInput
+              source="other_interests"
+              helperText="Add each interest on a new line"
+              multiline
+              validate={required("Add the influencer's interests")}
+            />
+          </TranslatableInputs>
+        </Box>
+      </Box>
+    </SimpleForm>
+  );
+}
+
+// MARK: Categories
+
+export function CategoriesSection() {
+  const record = useRecordContext<typeof certifiedInfluencers.$inferSelect>();
+  const relations = useGetManyReference<
+    typeof influencerCategories.$inferSelect
+  >(
+    "influencer_categories",
+    {
+      target: "influencer",
+      id: record?.influencer,
+    },
+    {
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  const selectedCategories = useGetMany<typeof categories.$inferSelect>(
+    "categories",
+    {
+      ids: relations.data?.map(({ category }) => category),
+    },
+    {
+      enabled: relations.data !== undefined,
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  const [search, setSearch] = useState("");
+  const [debouncedSearch] = useDebouncedValue(search, 700);
+
+  const searchOptions = useGetList<typeof categories.$inferSelect>(
+    "categories",
+    {
+      pagination: { page: 1, perPage: 5 },
+      filter: {
+        "id@not.in": `(${selectedCategories.data?.map(({ id }) => id).join(",")})`,
+        "title->>en@ilike": `*${debouncedSearch}*`,
+      },
+    },
+    {
+      enabled:
+        selectedCategories.data !== undefined && debouncedSearch.length > 0,
+    },
+  );
+
+  const [addRelation] = useCreate<typeof influencerCategories.$inferSelect>();
+  const [deleteRelations] =
+    useDelete<typeof influencerCategories.$inferSelect>();
+
+  const handleChange = async (
+    updatedCategories: (typeof categories.$inferSelect)[],
+  ) => {
+    const newItems = updatedCategories.filter(
+      (category) => !selectedCategories.data?.includes(category),
+    );
+    const removedItems = selectedCategories.data?.filter(
+      (category) => !updatedCategories.includes(category),
+    );
+
+    newItems.forEach((category) => void handleAdd(category));
+    removedItems?.forEach((category) => void handleRemove(category.id));
+  };
+
+  const handleAdd = async (category: typeof categories.$inferSelect) => {
+    if (!record) return;
+    await addRelation("influencer_categories", {
+      data: { influencer: record.influencer, category: category.id },
+    });
+    setSearch("");
+  };
+
+  const handleRemove = async (
+    categoryId: (typeof influencerCategories.$inferSelect)["id"],
+  ) => {
+    const relationId = relations.data?.find(
+      ({ category }) => category === categoryId,
+    )?.id;
+    if (!relationId) return;
+    void deleteRelations("influencer_categories", {
+      id: relationId,
+    });
+  };
+
+  return (
+    <Autocomplete
+      multiple
+      disableClearable
+      value={selectedCategories.data ?? []}
+      options={searchOptions.data ?? []}
+      onChange={(_, value) => {
+        void handleChange(value);
+      }}
+      inputValue={search}
+      getOptionLabel={(option) => option.title.en ?? ""}
+      onInputChange={(_, newInputValue) => {
+        setSearch(newInputValue);
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Cooperation Interests"
+          placeholder="Add Category"
+          variant="outlined"
+        />
+      )}
+      noOptionsText={
+        !debouncedSearch
+          ? "Type to search"
+          : searchOptions.isPending
+            ? "Fetchting..."
+            : "No options"
+      }
+    />
   );
 }
