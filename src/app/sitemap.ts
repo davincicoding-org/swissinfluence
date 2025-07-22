08fa87ff2afeba5f965fb56b70fc4aba0c7d9492
Michael Zeltner <michael.zeltner@me.com>
2025-07-22 13:39:15 +0200

    misc: improve SEO

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -13,20 +13,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
     ...getEntries("/convention", 0.9),
     ...getEntries("/network", 0.9),
     ...getEntries("/network/influencers", 0.8),
-    ...getEntries("/network/agencies", 0.8),
-    ...getEntries("/network/campaigns", 0.8),
-    ...getEntries("/network/events", 0.8),
-    ...getEntries("/academy", 0.8),
-    ...getEntries("/imprint", 0.3),
-    ...getEntries("/privacy", 0.3),
+    ...getEntries("/network/agencies", 0.7),
+    ...getEntries("/network/campaigns", 0.7),
+    ...getEntries("/network/events", 0.7),
+    ...getEntries("/academy", 0.7),
     ...getEntries("/nomination-process", 0.5),
     ...getEntries("/sponsoring", 0.5),
+    ...getEntries("/imprint", 0.3),
+    ...getEntries("/privacy", 0.3),
   ];
 }
 
-type Href = Parameters<typeof getPathname>[0]["href"];
-
-function getEntries(href: Href, priority?: number) {
+function getEntries(href: string, priority?: number) {
   return routing.locales.map((locale) => ({
     url: getUrl(href, locale),
     priority,
@@ -38,7 +36,7 @@ function getEntries(href: Href, priority?: number) {
   }));
 }
 
-function getUrl(href: Href, locale: Locale) {
+function getUrl(href: string, locale: Locale) {
   const pathname = getPathname({ locale, href });
   return host + pathname;
 }
