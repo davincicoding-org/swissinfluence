8304a55fee838b8dd96969bd5a244487195acd81
Michael Zeltner <michael.zeltner@me.com>
2025-08-04 00:54:21 +0200

    feat: improve voting error handling

--- src/server/voting.ts
+++ src/server/voting.ts
@@ -10,35 +10,39 @@ import { subscribeToNewsletter } from "./mailchimp";
 import { getPayloadClient } from "./payload";
 
 export async function submitVoting({
-  firstName,
-  lastName,
-  email,
-  award,
-  votes,
   newsletter,
+  ...submission
 }: VotingValues) {
   const payload = await getPayloadClient();
-  Sentry.setUser({
-    email,
-    firstName,
-    lastName,
-  });
 
-  await payload.create({
-    collection: "voting-submissions",
-    data: {
-      firstName,
-      lastName,
-      email,
-      award,
-      votes,
-      confirmed: false,
-      hash: crypto.randomUUID(),
-    },
-  });
+  const user = {
+    email: submission.email,
+    firstName: submission.firstName,
+    lastName: submission.lastName,
+  };
+
+  Sentry.setUser(user);
+
+  await payload
+    .create({
+      collection: "voting-submissions",
+      data: {
+        ...submission,
+        confirmed: false,
+        hash: crypto.randomUUID(),
+      },
+    })
+    .catch((error) => {
+      Sentry.captureException(
+        new Error("Failed to submit voting", { cause: error }),
+        {
+          data: submission,
+        },
+      );
+    });
 
   if (newsletter) {
-    await subscribeToNewsletter({ email, firstName, lastName });
+    await subscribeToNewsletter(user);
   }
 }
 
